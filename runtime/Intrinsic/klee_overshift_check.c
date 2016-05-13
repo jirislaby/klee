dd6dbd5f85be51178c9c22675ddacb4a040fb9f6
Jiri Slaby <jslaby@suse.cz>
2016-05-13 15:45:34 +0200

    klee_overshift_check: debug
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

--- runtime/Intrinsic/klee_overshift_check.c
+++ runtime/Intrinsic/klee_overshift_check.c
@@ -18,12 +18,19 @@
  * then we can detect overshifting (which has undefined behaviour).
  */
 void klee_overshift_check(unsigned long long bitWidth, unsigned long long shift) {
+  extern int printf (const char *__restrict __format, ...);
   if (shift >= bitWidth) {
     /* Maybe we shouldn't throw an error because
      * overshifting can be non-fatal? Perhaps
      * we should generate a test case but carry
      * on executing the state with a warning?
      */
+    if (klee_is_symbolic(shift)) {
+	    printf("overflow BW=%llu < concr_S=%llu sym_S will follow\n",
+			    bitWidth, klee_get_valuell(shift));
+	    klee_print_expr("overflow", shift);
+    } else
+	    printf("overflow BW=%llu < S=%llu\n", bitWidth, shift);
     klee_report_error("IGNORED", 0 /*Ignored */, "overshift error", "overshift.err");
   }
 }
